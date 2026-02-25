c9793e1d074879d0b5273fa56c6277c8c166e595
Billy Hawkes <billyhawkes02@gmail.com>
2026-02-25 13:53:54 -0500

    task: remove old godot stuff

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -231,29 +231,6 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 
-print 'Hello from godot!'
--- paths to check for project.godot file
-local paths_to_check = { '/', '/../' }
-local is_godot_project = false
-local godot_project_path = ''
-local cwd = vim.fn.getcwd()
-
--- iterate over paths and check
-for key, value in pairs(paths_to_check) do
-	if vim.uv.fs_stat(cwd .. value .. 'project.godot') then
-		is_godot_project = true
-		godot_project_path = cwd .. value
-		break
-	end
-end
-
--- check if server is already running in godot project path
-local is_server_running = vim.uv.fs_stat(godot_project_path .. '/server.pipe')
--- start server, if not already running
-if is_godot_project and not is_server_running then
-	vim.fn.serverstart(godot_project_path .. '/server.pipe')
-end
-
 require('lazy').setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
