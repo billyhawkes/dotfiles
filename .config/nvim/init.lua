eeaaaf1ec549f4f6c7818a4d9ab280e065134220
Billy Hawkes <billyhawkes02@gmail.com>
2026-04-16 09:25:14 -0400

    task: autocomplete icons

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -792,6 +792,41 @@ require('lazy').setup({
 				-- By default, you may press `<c-space>` to show the documentation.
 				-- Optionally, set `auto_show = true` to show the documentation after a delay.
 				documentation = { auto_show = true, auto_show_delay_ms = 200 },
+				menu = {
+					draw = {
+						components = {
+							kind_icon = {
+								text = function(ctx)
+									local icon = ctx.kind_icon
+									if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+										local dev_icon, _ = require('nvim-web-devicons').get_icon(ctx.label)
+										if dev_icon then
+											icon = dev_icon
+										end
+									else
+										icon = require('lspkind').symbol_map[ctx.kind] or ''
+									end
+
+									return icon .. ctx.icon_gap
+								end,
+
+								-- Optionally, use the highlight groups from nvim-web-devicons
+								-- You can also add the same function for `kind.highlight` if you want to
+								-- keep the highlight groups in sync with the icons.
+								highlight = function(ctx)
+									local hl = ctx.kind_hl
+									if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+										local dev_icon, dev_hl = require('nvim-web-devicons').get_icon(ctx.label)
+										if dev_icon then
+											hl = dev_hl
+										end
+									end
+									return hl
+								end,
+							},
+						},
+					},
+				},
 			},
 
 			sources = {
